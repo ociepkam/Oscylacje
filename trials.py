6a2b3c2f862202ffe698786f03d0d7f644ac9f5b
ociepkam <ociepka.m@gmail.com>
2016-03-30 15:44:51 +0200

    Added files via upload

--- trials.py
+++ trials.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = 'ociepkam'
 
-import csv
 import yaml
 import random
 from openpyxl import Workbook
+
+__author__ = 'ociepkam'
 
 
 class Trials:
@@ -46,24 +46,26 @@ class Trials:
             yamlfile.write(yaml.dump(self.list_of_trials))
 
     def save_to_xlsx(self, filename):
-        veryfication = "=IF(AND(OR(A{0} =\"letters\",A{0} =\"figures\",A{0} =\"NamesHeightRelations\", A{0} =\"NamesAgeRelations\"),OR(B{0} = 2,B{0} = 3,B{0} = 4),OR(D{0} = 0,D{0} = 1),OR(E{0} = 0,E{0} = 1),OR(I{0} = 0,I{0} = 1),OR(K{0} = 0,K{0} = 1),OR(L{0} = 0,L{0} = 1),OR(N{0} = 0,N{0} = 1),OR(O{0} = 0,O{0} = 1)),1, 0)"
-        global_veryfication = "=SUM(Q2: Q500)"
+
+        verification = "=IF(AND(OR(A{0} =\"letters\",A{0} =\"figures\",A{0} =\"NamesHeightRelations\", A{0} =\"NamesAgeRelations\"),OR(B{0} = 2,B{0} = 3,B{0} = 4),OR(D{0} = 0,D{0} = 1),OR(E{0} = 0,E{0} = 1),OR(I{0} = 0,I{0} = 1),OR(K{0} = 0,K{0} = 1),OR(L{0} = 0,L{0} = 1),OR(N{0} = 0,N{0} = 1),OR(O{0} = 0,O{0} = 1)),1, 0)"
+        global_verification = "=SUM(Q2: Q500)"
 
         wb = Workbook()
 
         # grab the active worksheet
         ws = wb.active
 
+
         # Data can be assigned directly to cells
         ws.append(
             ['SAMPLE_TYPE', 'N', 'NR', 'MEMORY', 'INTEGR', 'SHOW_TIME', 'RESP_TIME', 'MAXTIME', 'FEEDB', 'FEEDB_TIME',
-             'WAIT', 'EXP', 'FIXTIME', 'EEG', 'LIST_VIEW', '', global_veryfication])
+             'WAIT', 'EXP', 'FIXTIME', 'EEG', 'LIST_VIEW', '', global_verification])
 
         for idx, trial_info in enumerate(self.list_of_trials):
             ws.append([trial_info['SAMPLE_TYPE'], trial_info['N'], trial_info['NR'], trial_info['MEMORY'],
                        trial_info['INTEGR'], trial_info['SHOW_TIME'], trial_info['RESP_TIME'], trial_info['MAXTIME'],
                        trial_info['FEEDB'], trial_info['FEEDB_TIME'], trial_info['WAIT'], trial_info['EXP'],
-                       trial_info['FIXTIME'], trial_info['EEG'], trial_info['LIST_VIEW'], '' ,veryfication.format(idx+2)])
+                       trial_info['FIXTIME'], trial_info['EEG'], trial_info['LIST_VIEW'], '' ,verification.format(idx+2)])
 
         # Save the file
         wb.save(filename + ".xlsx")
